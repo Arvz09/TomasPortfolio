e4514056cceec3eba5682e50f968b87d79ac8456
Arvz09 <arbietomas@gmail.com>
2026-01-07 14:14:24 +0800

    Header removed

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
 import Index from "./pages/Index";
 import Certifications from "./pages/Certifications";
 import TechStack from "./pages/TechStack";
@@ -18,7 +17,6 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Header />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/certifications" element={<Certifications />} />
